71457df3e9545d4a51b952b8e1a3ddcd3fb98b51
LAPTOP-774063JM\ASUS <boatsuban2543@gmail.com>
2021-10-03 17:08:14 +0700

    book vue

---
+++ src/components/BookItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="book-item">
+         <h1>{{book.title}}</h1><br/>
+         <img v-bind:src="book.thumbnailUrl" /><br/>
+         <b>Author:</b>{{book.author}} <br/>
+         <b>Category:</b>{{book.category}} <br/>
+         <b>ISBN:</b>{{book.isbn}} <br/>
+         <b>Pages:</b>{{book.pageCount}} <br/>
+         <b>Description:</b><br/>
+         <p>{{book.shortDescription}} </p>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: "BookItem",
+  components: {
+     
+  },
+ props: ["book"]
+  
+}
+</script>
+
+<style>
+.book-item{
+    overflow: auto;
+    text-align: center;
+    width: 100%;
+    background:rgba(236, 231, 231, 0.959);
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+
+}
+</style>
